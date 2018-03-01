ad3b3ad1c0d2d5c32e7525412845458bf2656413
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:42:19 +0000

    Whitelist columns in Descriptor for ActiveAdmin

--- app/admin/descriptors.rb
+++ app/admin/descriptors.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Descriptor do
+
+ permit_params :tag_id, :restaurant_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
