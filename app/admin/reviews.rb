6748e7edb685c5127d230a901fbb8ad2c0087819
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:42:12 +0000

    Whitelist columns in Review for ActiveAdmin

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Review do
+
+ permit_params :restaurant_id, :user_id, :description, :stars
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
