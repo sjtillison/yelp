d49dd9dd16d2bd7eaa8e59dea325548e027b9c44
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:42:28 +0000

    Whitelist columns in Reply for ActiveAdmin

--- app/admin/replies.rb
+++ app/admin/replies.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Reply do
+
+ permit_params :owner_id, :review_id, :content
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
