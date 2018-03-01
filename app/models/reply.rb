12e299541a78f7e0d73eb8110b40e9df4ddea118
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:41:50 +0000

    Direct association between users and replies

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,9 @@
 class Reply < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :foreign_key => "owner_id"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :replies,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   has_many   :restaurant_owners,
              :class_name => "Ownership",
              :dependent => :destroy
