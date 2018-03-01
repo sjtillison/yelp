c9de588c7d1a9428f210d29850501f1ce33a68e4
Raghu Betina <raghu@firstdraft.com>
2018-03-01 20:41:50 +0000

    Direct association between ownerships and replies

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,6 +1,10 @@
 class Ownership < ApplicationRecord
   # Direct associations
 
+  has_many   :replies,
+             :foreign_key => "owner_id",
+             :dependent => :destroy
+
   belongs_to :restaurant
 
   belongs_to :user
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,9 @@
 class Reply < ApplicationRecord
   # Direct associations
 
+  belongs_to :owner,
+             :class_name => "Ownership"
+
   belongs_to :user,
              :foreign_key => "owner_id"
 
